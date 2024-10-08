4463ae346b611ec60abb6eedb538903bd1b65288
Nathan Souza <nathan.souza@a3data.com.br>
2024-10-08 13:31:46 -0300

    fix: cria arquivo push_image lambda

---
+++ deployment/scripts/push_image_lambda.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+DOCKER_IMAGE_NAME=$1
+AWS_REGION=$2
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+REPO_URI="$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$DOCKER_IMAGE_NAME"
+
+docker build -t $DOCKER_IMAGE_NAME -f deployment/docker/Dockerfile.lambda .
+docker tag "$DOCKER_IMAGE_NAME:latest" "$REPO_URI:latest"
+aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$REPO_URI"
+docker push "$REPO_URI:latest"
