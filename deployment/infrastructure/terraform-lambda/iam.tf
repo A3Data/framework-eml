eb21bec874ab12de70b7cf7e259b9e29ba269bc1
Nathan Souza <nathan.souza@a3data.com.br>
2024-10-08 12:20:29 -0300

    fix: ajusta terraform

--- deployment/infrastructure/terraform-lambda/iam.tf
+++ deployment/infrastructure/terraform-lambda/iam.tf
@@ -1,4 +1,9 @@
+data "aws_iam_role" "existing_role" {
+  name = "prediction_eml_role"
+}
+
 resource "aws_iam_role" "prediction_eml_role" {
+  count              = length(data.aws_iam_role.existing_role.*.name) == 0 ? 1 : 0
   name               = "prediction_eml_role"
 
   assume_role_policy = jsonencode({
@@ -17,7 +22,8 @@ resource "aws_iam_role" "prediction_eml_role" {
 }
 
 resource "aws_iam_policy_attachment" "prediction_eml_policy_attachment" {
+  count      = length(data.aws_iam_role.existing_role.*.name) == 0 ? 1 : 0
   name       = "prediction_eml_policy_attachment"
-  roles      = [aws_iam_role.prediction_eml_role.name]
+  roles      = [aws_iam_role.prediction_eml_role[0].name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
