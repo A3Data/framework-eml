a82d441762ea55532800d3b6e1306dfdef65ac06
Nathan Souza <nathan.souza@a3data.com.br>
2024-10-08 14:13:47 -0300

    feat: adiciona pipeline de destroy

---
+++ deployment/scripts/destroy_infra_lambda.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+BUCKET_NAME="$ACCOUNT_ID-prediction-eml"
+AWS_REGION=$1
+
+# Verifica se o bucket existe
+if aws s3api head-bucket --bucket "$BUCKET_NAME" 2>/dev/null; then
+    echo "O bucket '$BUCKET_NAME' já existe."
+else
+    # Se o bucket não existir, cria o bucket
+    aws s3api create-bucket --bucket "$BUCKET_NAME" --region us-east-1
+    echo "Bucket '$BUCKET_NAME' criado com sucesso."
+fi
+
+# Inicializa o Terraform
+terraform -chdir="deployment/infrastructure/terraform-lambda" init -backend-config="bucket=$BUCKET_NAME" -backend-config="region=$AWS_REGION"
+
+# Comando para destruir a infraestrutura
+terraform -chdir="deployment/infrastructure/terraform-lambda" destroy -auto-approve
