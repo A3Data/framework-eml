15f599a88a0b5bece90207796625933ef5b91920
Nathan Souza <nathan.souza@a3data.com.br>
2024-10-08 12:56:43 -0300

    fix: remove config duplicada

--- deployment/infrastructure/terraform-lambda/provider.tf
+++ deployment/infrastructure/terraform-lambda/provider.tf
@@ -1,24 +1,5 @@
 provider "aws" {
   region  = var.region
 }
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.49"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 3.0"
-    }
-  }
-  backend "s3" {
-    key     = "terraform/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
-
 
 data "aws_caller_identity" "current" {}
